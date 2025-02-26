d44a7c669b9c26cdad7794d93446be61db54cbec
Matt <matt9753@proton.me>
2025-02-26 20:39:26 -0300

    adicionado mais testes

--- test/Main.hs
+++ test/Main.hs
@@ -2,8 +2,11 @@ module Main (main) where
 import Test.HUnit
 import ClassifierTest (classifierTests)
 import ModelTest (modelTests)
+import PreprocessingTest (preprocessingTests)
+import TrainingTest (trainingTests)
+
 tests :: Test
-tests = TestList [classifierTests, modelTests]
+tests = TestList [classifierTests, modelTests, preprocessingTests, trainingTests]
 
 main :: IO Counts
 main = runTestTT tests
--- test/PreprocessingTest.hs
+++ test/PreprocessingTest.hs
@@ -1,6 +1,30 @@
 -- Testes para o pré-processamento de dados
 
-module PreprocessingTest (main) where
+module PreprocessingTest where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+import Test.HUnit
+import Preprocessing
+
+-- Teste para tokenização simples
+testTokenizeSimple :: Test
+testTokenizeSimple = TestCase $ 
+    assertEqual "Tokenizing 'Hello world'" 
+                 ["Hello", "world"]
+                 (tokenize "Hello world")
+
+-- Teste para remoção de pontuação
+testTokenizePunctuation :: Test
+testTokenizePunctuation = TestCase $ 
+    assertEqual "Tokenizing 'Hello, world!'" 
+                 ["Hello", "world"]
+                 (tokenize "Hello, world!")
+
+-- Teste para múltiplos espaços
+testTokenizeMultipleSpaces :: Test
+testTokenizeMultipleSpaces = TestCase $ 
+    assertEqual "Tokenizing '  Hello   world  '" 
+                 ["Hello", "world"]
+                 (tokenize "  Hello   world  ")
+
+preprocessingTests :: Test
+preprocessingTests = TestList [testTokenizeSimple, testTokenizePunctuation, testTokenizeMultipleSpaces]
